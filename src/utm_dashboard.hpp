d119349b5457565d6e80915f242f68916b37f36b
khromenokroman <roma55592@yandex.ru>
2026-04-12 08:47:22 +0200

    удален неиспользуемый код

--- src/utm_dashboard.hpp
+++ src/utm_dashboard.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include <nlohmann/json.hpp>
-#define CPPHTTPLIB_THREAD_POOL_COUNT 4
 #include <httplib.h>
 
 class UTMDashboard {
@@ -18,8 +17,6 @@ public:
 private:
     static std::time_t parse_date_time(const std::string &s);
     static std::string expire_class(const std::string &expireDate);
-    static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp);
-    std::string http_get(std::string const &url);
     std::string get_detail_utm(std::string_view ip, std::string_view name);
 
     httplib::Server m_server; // 752
